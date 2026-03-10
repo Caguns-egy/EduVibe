9c93c51a9e7cdca35c2b535fce39dc439f617522
Ahmed Montaser <amontaser908@gmail.com>
2026-03-10 04:18:15 +0200

    Fix base path for production only

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/EduVibe/',
+  base: process.env.NODE_ENV === 'production' ? '/EduVibe/' : '/',
   plugins: [react()],
 })
